2c79c6f055e5fbe057031e0d4810852eb6124222
= <=>
2024-05-21 17:00:53 -0300

    main

--- App.tsx
+++ App.tsx
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { AntDesign } from '@expo/vector-icons';
+
+import { HomeScreen } from './src/pages/HomeScreen';
+import { NotificationsScreen } from './src/pages/NotificationsScreen';
+import { AdicionarEquipamento } from './src/pages/AdicionarEquipamento';
+
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Adicionar Equipamento" component={AdicionarEquipamento} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor:'#f0f4ff',
+      paddingHorizontal:18
+    },
+    title:{
+      fontSize:34,
+      marginBottom:34,
+      color: '#121212',
+      fontWeight:'bold'
+    },
+    input:{
+      width:'100%',
+      height: 40,
+      backgroundColor:'#ffffff',
+      paddingHorizontal: 8,
+      marginBottom: 8,
+      borderRadius: 4,
+      color:'#121212'
+    },
+    button:{
+      width:'100%',
+      height: 60,
+      backgroundColor: '#45d800',
+      borderRadius: 8,
+      marginBottom: 8,
+      justifyContent:'center',
+      alignItems: 'center',
+    },
+    buttonText:{
+      color:'#fff',
+      justifyContent:'center',
+      fontWeight:'bold',
+      fontSize: 18
+    },
+    labelError:{
+      width: '100%',
+      alignSelf:'flex-start',
+      color:'#ff375b',
+      marginBottom: 8, 
+    }
+})
---
+++ src/pages/AdicionarEquipamento/index.tsx
@@ -0,0 +1,218 @@
+import * as React from 'react';
+import { Button, View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
+const schema = yup.object({
+  patrimonio: yup.string().required("Informe o Patrimônio!"),
+  setor: yup.string().required("Informe o Setor!"),
+  limpezasemanal: yup.string().required("Informe a Data da Limpeza!"),
+  datapreventiva: yup.string().required("Informe a Data da Preventiva!"),
+  proximapreventiva: yup.string().required("Informe a Data da Próxima Preventiva!"),
+  obs: yup.string().required("Informe alguma Observação!")
+})
+
+export function AdicionarEquipamento({ navigation }) {
+  const { control, handleSubmit, formState: {errors}} = useForm ({
+    resolver: yupResolver(schema)
+
+  })
+
+
+  function handleSignin (data){
+    console.log(data);
+  }
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Preencha os Campos!</Text>
+
+      <Controller
+        control={control}
+        name= "patrimonio"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput 
+            style={ [
+              styles.input, {
+                borderWidth: errors.patrimonio && 1,
+                borderColor: errors.patrimonio && '#ff375b'
+
+            }]}
+            onChangeText={onChange}
+            onBlur= {onBlur}
+            value= {value}
+            placeholder='Patrimônio'
+          />
+        )}
+
+      />
+        {errors.patrimonio && <Text style={styles.labelError}>{errors.patrimonio?.message}</Text>}
+
+        <Controller
+        control={control}
+        name= "setor"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput 
+            style={ [
+              styles.input, {
+                borderWidth: errors.setor && 1,
+                borderColor: errors.setor && '#ff375b'
+
+            }]}
+            onChangeText={onChange}
+            onBlur= {onBlur}
+            value= {value}
+            placeholder='Setor'
+          />
+        )}
+
+      />
+        {errors.setor && <Text style={styles.labelError}>{errors.setor?.message}</Text>}
+
+        <Controller
+        control={control}
+        name= "limpezasemanal"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput 
+            style={ [
+              styles.input, {
+                borderWidth: errors.limpezasemanal && 1,
+                borderColor: errors.limpezasemanal && '#ff375b'
+
+            }]}
+            onChangeText={onChange}
+            onBlur= {onBlur}
+            value= {value}
+            placeholder='Data da Limpeza Semanal'
+          />
+        )}
+
+      />
+        {errors.limpezasemanal && <Text style={styles.labelError}>{errors.limpezasemanal?.message}</Text>}
+
+        <Controller
+        control={control}
+        name= "datapreventiva"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput 
+            style={ [
+              styles.input, {
+                borderWidth: errors.datapreventiva && 1,
+                borderColor: errors.datapreventiva && '#ff375b'
+
+            }]}
+            onChangeText={onChange}
+            onBlur= {onBlur}
+            value= {value}
+            placeholder='Data da Preventiva'
+          />
+        )}
+
+      />
+        {errors.datapreventiva && <Text style={styles.labelError}>{errors.datapreventiva?.message}</Text>}
+
+        <Controller
+        control={control}
+        name= "proximapreventiva"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput 
+            style={ [
+              styles.input, {
+                borderWidth: errors.proximapreventiva && 1,
+                borderColor: errors.proximapreventiva && '#ff375b'
+
+            }]}
+            onChangeText={onChange}
+            onBlur= {onBlur}
+            value= {value}
+            placeholder='Próxima Preventiva'
+          />
+        )}
+
+      />
+        {errors.proximapreventiva && <Text style={styles.labelError}>{errors.proximapreventiva?.message}</Text>}
+
+        <Controller
+        control={control}
+        name= "obs"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput 
+            style={ [
+              styles.input, {
+                borderWidth: errors.obs && 1,
+                borderColor: errors.obs && '#ff375b'
+
+            }]}
+            onChangeText={onChange}
+            onBlur= {onBlur}
+            value= {value}
+            placeholder='Observações'
+          />
+        )}
+
+      />
+        {errors.obs && <Text style={styles.labelError}>{errors.obs?.message}</Text>}
+  
+
+      <TouchableOpacity style={styles.button} onPress={handleSubmit(handleSignin)}>
+        <Text style={styles.buttonText}>Enviar</Text>
+        <Feather name="send" size={24} color="#fff" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.buttonText}>Home</Text>
+        <AntDesign name="home" size={24} color="#fff" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#f0f4ff',
+    paddingHorizontal:18
+  },
+  title:{
+    fontSize:34,
+    marginBottom:34,
+    color: '#121212',
+    fontWeight:'bold'
+  },
+  input:{
+    width:'100%',
+    height: 40,
+    backgroundColor:'#ffffff',
+    paddingHorizontal: 8,
+    marginBottom: 8,
+    borderRadius: 4,
+    color:'#121212'
+  },
+  button:{
+    width:'100%',
+    height: 60,
+    backgroundColor: '#45d800',
+    borderRadius: 8,
+    marginBottom: 8,
+    justifyContent:'center',
+    alignItems: 'center',
+  },
+  buttonText:{
+    color:'#fff',
+    justifyContent:'center',
+    fontWeight:'bold',
+    fontSize: 18
+  },
+  labelError:{
+    width: '100%',
+    alignSelf:'flex-start',
+    color:'#ff375b',
+    marginBottom: 8, 
+  }
+
+})
